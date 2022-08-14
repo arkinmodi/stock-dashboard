cee923e3c6bd3393cf3d599864c4533acf525634
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-08-14 17:30:41 -0400

    create base stock component

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,6 +23,14 @@ const Home: NextPage = () => {
     }
   }, [stocks, response]);
 
+  const handleDeleteCard = (stock: string, index: number) => {
+    setResponse(`🗑 Removed ${stock}`);
+    setStocks([
+      ...stocks.slice(0, index),
+      ...stocks.slice(index + 1, stocks.length),
+    ]);
+  };
+
   return (
     <div>
       <Head>
@@ -61,24 +69,49 @@ const Home: NextPage = () => {
           <button>Add Stock</button>
         </form>
         <p>{response}</p>
-        <ul></ul>
-        {stocks.map((stock, index) => (
-          <li
-            key={stock}
-            onClick={() => {
-              setResponse(`🗑 Removed ${stock}`);
-              setStocks([
-                ...stocks.slice(0, index),
-                ...stocks.slice(index + 1, stocks.length),
-              ]);
-            }}
-          >
-            {stock}
-          </li>
-        ))}
+        <div>
+          {stocks.map((stock, index) => (
+            <StockCard
+              key={index}
+              ticker={stock}
+              deleteCard={() => handleDeleteCard(stock, index)}
+            />
+          ))}
+        </div>
       </main>
     </div>
   );
+};
+
+const StockCard: React.FC<{ ticker: string; deleteCard: () => void }> = (
+  props
+) => {
+  const stockData = getStockData(props.ticker);
+
+  return (
+    <div>
+      <h1>{props.ticker}</h1>
+      <div>
+        {stockData.currencySymbol}
+        {stockData.currentPrice}
+      </div>
+      <div>
+        {stockData.currencySymbol}
+        {stockData.opening}
+      </div>
+      <div>{stockData.dailyPercentChange * 100}%</div>
+      <button onClick={props.deleteCard}>❌</button>
+    </div>
+  );
+};
+
+const getStockData = (ticker: string) => {
+  return {
+    currencySymbol: "$",
+    opening: 100,
+    dailyPercentChange: 0.1,
+    currentPrice: 69.69,
+  };
 };
 
 export default Home;
