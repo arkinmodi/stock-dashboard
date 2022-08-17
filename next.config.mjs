58f29599d23dbe6cd8563ab36479cad1348800c6
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-08-16 22:03:49 -0400

    make es module compliant

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 import { env } from "./src/env/server.mjs";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+/**
+ * Don't be scared of the generics here.
+ * All they do is to give us autocompletion when using this.
+ *
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
+function defineNextConfig(config) {
+  return config;
 }
 
-module.exports = nextConfig
+export default defineNextConfig({
+  reactStrictMode: true,
+  swcMinify: true,
+});
