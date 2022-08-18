eae1ba6ef5fe9b9800ead975815378a045111ed1
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-08-17 21:17:51 -0400

    add prisma to context

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,8 +1,16 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 
+import { prisma } from "@server/db/client";
+
 export const createContext = (opts: trpcNext.CreateNextContextOptions) => {
-  return {};
+  const req = opts.req;
+  const res = opts.res;
+  return {
+    req,
+    res,
+    prisma,
+  };
 };
 
 export type Context = trpc.inferAsyncReturnType<typeof createContext>;
