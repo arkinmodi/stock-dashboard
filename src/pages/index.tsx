a152d6e53015f50a8116c90ca9b316ff85112f56
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-08-21 21:05:01 -0400

    prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,23 +19,24 @@ const Home: NextPage = () => {
   const [animationStocks] = useAutoAnimate<HTMLDivElement>();
   const [animationResponse] = useAutoAnimate<HTMLParagraphElement>();
 
-    const {data} = trpc.proxy.user.getSavedStocks.useQuery(undefined, {
-      refetchInterval: false,
-      refetchOnReconnect: false,
-      refetchOnWindowFocus: false,
-      retry: false,
-    });
+  const { data } = trpc.proxy.user.getSavedStocks.useQuery(undefined, {
+    refetchInterval: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+    retry: false,
+  });
 
-    const {mutate} = trpc.proxy.user.updateSavedStocks.useMutation({
-      retry: 3
-    });
+  const { mutate } = trpc.proxy.user.updateSavedStocks.useMutation({
+    retry: 3,
+  });
 
   // Loads the stocks in local storage on mount
   useEffect(() => {
-
     let savedStocks;
     if (status === "loading" || status === "unauthenticated") {
-      savedStocks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) ?? "[]") as string[];
+      savedStocks = JSON.parse(
+        localStorage.getItem(LOCAL_STORAGE_KEY) ?? "[]",
+      ) as string[];
     } else {
       savedStocks = data ?? [];
     }
@@ -53,14 +54,13 @@ const Home: NextPage = () => {
   useEffect(() => {
     // Check if we are doing the initial load
     if (response !== "" && response !== GETTING_STARTED_MESSAGE) {
-
       if (status === "loading" || status === "unauthenticated") {
         localStorage.setItem(
           LOCAL_STORAGE_KEY,
           JSON.stringify(Array.from(stocksSet.values())),
         );
       } else {
-        mutate({stocks: stocksSet});
+        mutate({ stocks: stocksSet });
       }
 
       console.log("Saving... Auth Status = ", status);
@@ -104,22 +104,22 @@ const Home: NextPage = () => {
             e.preventDefault();
             handleAddStockEvent();
           }}
-          className="flex flex-row justify-center mt-4"
+          className="mt-4 flex flex-row justify-center"
         >
           <input
             type="text"
             placeholder="e.g., AAPL"
             value={input}
             onChange={(e) => setInput(e.currentTarget.value)}
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-100 py-2 px-4 mr-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            className="w-100 mr-4 appearance-none rounded border-2 border-gray-200 bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-purple-500 focus:bg-white focus:outline-none"
           ></input>
-          <button className="shadow bg-purple-600 hover:bg-purple-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+          <button className="focus:shadow-outline rounded bg-purple-600 py-2 px-4 font-bold text-white shadow hover:bg-purple-500 focus:outline-none">
             Add Stock
           </button>
         </form>
         <p
           ref={animationResponse}
-          className="text-center mt-4 font-mono whitespace-pre-wrap"
+          className="mt-4 whitespace-pre-wrap text-center font-mono"
         >
           {response}
         </p>
@@ -160,15 +160,15 @@ const StockCard: React.FC<{
       refetchOnWindowFocus: false,
       retry: false,
       onError: (e) => e.data?.code === "NOT_FOUND" && setNotFound(true),
-    }
+    },
   );
 
   const failedToLoadData =
     isError && error.data?.code == "INTERNAL_SERVER_ERROR";
 
   return (
-    <div className="min-w-96 min-h-[150px] bg-neutral-50 drop-shadow rounded-xl p-3 m-4">
-      <div className="flex flex-row justify-center relative">
+    <div className="min-w-96 m-4 min-h-[150px] rounded-xl bg-neutral-50 p-3 drop-shadow">
+      <div className="relative flex flex-row justify-center">
         <h2 className="text-center text-lg font-bold">{props.ticker}</h2>
         <button
           onClick={props.handleDeleteCardEvent}
@@ -178,35 +178,35 @@ const StockCard: React.FC<{
         </button>
       </div>
       {failedToLoadData && !data && (
-        <div className="w-96 flex flex-wrap justify-center items-center relative mt-5">
-          <p className="font-bold text-2xl">Error Loading Data</p>
+        <div className="relative mt-5 flex w-96 flex-wrap items-center justify-center">
+          <p className="text-2xl font-bold">Error Loading Data</p>
         </div>
       )}
       {!data && !failedToLoadData && (
-        <div className="w-96 flex flex-wrap justify-center items-center relative mt-5">
+        <div className="relative mt-5 flex w-96 flex-wrap items-center justify-center">
           <Image src="/three-dots.svg" width={55} height={55} alt="Loading" />
         </div>
       )}
       {data && !failedToLoadData && (
-        <div className="flex flex-wrap justify-center items-center relative mt-5">
-          <div className="text-center ml-1 mr-2">
+        <div className="relative mt-5 flex flex-wrap items-center justify-center">
+          <div className="ml-1 mr-2 text-center">
             <p className="font-bold">Daily High</p>
-            <p className="font-bold text-2xl">
+            <p className="text-2xl font-bold">
               {data.currencySymbol}
               {data.dailyHigh.toFixed(2)}
             </p>
           </div>
-          <div className="text-center ml-2 mr-2">
+          <div className="ml-2 mr-2 text-center">
             <p className="font-bold">Current Price</p>
-            <p className="font-bold text-4xl">
+            <p className="text-4xl font-bold">
               {data.currencySymbol}
               {data.currentPrice.toFixed(2)}
             </p>
           </div>
-          <div className="text-center ml-2 mr-1">
+          <div className="ml-2 mr-1 text-center">
             <p className="font-bold">% Change</p>
             <p
-              className={`font-bold text-xl ${
+              className={`text-xl font-bold ${
                 data.dailyPercentChange < 0 ? "text-red-500" : "text-green-500"
               }`}
             >
@@ -221,44 +221,62 @@ const StockCard: React.FC<{
 };
 
 const NavBar: React.FC<{}> = () => {
-  const {data: session, status} = useSession();
+  const { data: session, status } = useSession();
   return (
     <>
-      <nav className="bg-neutral-50 border-gray-200 px-2 sm:px-4 py-2.5 drop-shadow-sm">
-        <div className="container flex flex-wrap justify-between items-center mx-auto">
-          <span className="self-center text-xl font-semibold whitespace-nowrap">
+      <nav className="border-gray-200 bg-neutral-50 px-2 py-2.5 drop-shadow-sm sm:px-4">
+        <div className="container mx-auto flex flex-wrap items-center justify-between">
+          <span className="self-center whitespace-nowrap text-xl font-semibold">
             Stock Dashboard
           </span>
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-              <ul className="flex flex-col p-2 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
-                {(status === "unauthenticated" || status === "loading") && (
+            <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-2 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium">
+              {(status === "unauthenticated" || status === "loading") && (
                 <li>
-                  <button onClick={() => signIn()} className="flex flex-row justify-center items-center">
-                    <Image src="/google-logo.svg" height={50} width={50} alt="" />
+                  <button
+                    onClick={() => signIn()}
+                    className="flex flex-row items-center justify-center"
+                  >
+                    <Image
+                      src="/google-logo.svg"
+                      height={50}
+                      width={50}
+                      alt=""
+                    />
                     <p className="pl-2">Sign in with Google</p>
                   </button>
                 </li>
-                )}
-                {status === "authenticated" && session && (
-                  <li>
-                    <button onClick={() => signOut()} className="flex flex-row justify-center items-center">
-                      <Image src={session.user?.image ?? ""} height={50} width={50} alt="" style={{borderRadius: 50}} />
-                      <p className="px-2">{session.user?.name}</p>
-                    </button>
-                  </li>
-                )}
-              </ul>
-              </div>
+              )}
+              {status === "authenticated" && session && (
+                <li>
+                  <button
+                    onClick={() => signOut()}
+                    className="flex flex-row items-center justify-center"
+                  >
+                    <Image
+                      src={session.user?.image ?? ""}
+                      height={50}
+                      width={50}
+                      alt=""
+                      style={{ borderRadius: 50 }}
+                    />
+                    <p className="px-2">{session.user?.name}</p>
+                  </button>
+                </li>
+              )}
+            </ul>
+          </div>
         </div>
       </nav>
       {(status === "unauthenticated" || status === "loading") && (
-        <div className="bg-yellow-500 px-2 sm:px-4 py-0.5 items-center justify-center flex flex-row">
-          <span className="text-center text-black font-mono text-sm">
-              Working Locally In Guest Mode. Sign In With Google To Use Cloud Saves.
+        <div className="flex flex-row items-center justify-center bg-yellow-500 px-2 py-0.5 sm:px-4">
+          <span className="text-center font-mono text-sm text-black">
+            Working Locally In Guest Mode. Sign In With Google To Use Cloud
+            Saves.
           </span>
         </div>
-        )}
-      </>
+      )}
+    </>
   );
 };
 
